c2a2e66fc0ba62fa49de8c123006efb3deed24f8
raditya-rizky <gusrizkydharmaputra123@gmail.com>
2022-08-13 23:57:29 +0800

    found som bugs while refreshing the page

---
+++ pages/my-profile.jsx
@@ -0,0 +1,70 @@
+import { Button } from "react-bootstrap";
+import style from "../styles/MyProfile.module.css";
+import cn from "classnames";
+import Head from "next/head";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+
+export default function MyProfile() {
+  const { data: session } = useSession();
+
+  return (
+    <>
+      <Head>
+        <title>Your Profile</title>
+      </Head>
+      <div className={cn(style.container, "container-fluid")}>
+        <div className="row pt-5">
+          <div className="col-lg-5">
+            <div className="left text-light">
+              <p className="text-center">Here&apos;s your profile.</p>
+              <p className="text-center">
+                You can edit by clicking the &quot;Edit&quot; button.
+              </p>
+              <Image
+                width="500em"
+                height="550em"
+                src="/images/cartoon.png"
+                alt="cartoon"
+                className="img-fluid"
+                style={{ width: "35em" }}
+              />
+            </div>
+          </div>
+          <div className="col-lg text-light">
+            <div className={style.right}>
+              <div className="container p-4">
+                <h2 className="text-center">YOUR PROFILE</h2>
+                <br />
+                <div>
+                  <h5>Email:</h5>
+                  <p>{session.user.email}</p>
+                  <h5>Username:</h5>
+                  <p>{session.user.username}</p>
+                  <h5>Total Score:</h5>
+                  <p>{session.user.total_score}</p>
+                  <h5>Bio:</h5>
+                  <p>{session.user.bio}</p>
+                  <h5>Social Media:</h5>
+                  <p>{session.user.social_media_url}</p>
+                  <h5>City:</h5>
+                  <p>{session.user.city}</p>
+                  <h5>Joined At:</h5>
+                  <p>{new Date(session.user.createdAt).toLocaleDateString()}</p>
+                </div>
+                <div className="row justify-content-center mt-3">
+                  <Link href="/edit-profile" passHref>
+                    <Button type="button" className={style.loginButton}>
+                      Edit
+                    </Button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
